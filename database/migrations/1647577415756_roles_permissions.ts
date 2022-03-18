34cbd76093e64bf45c63303b61c484a28b91a7fa
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-03-18 01:28:23 -0300

    feat: make roles permissions migration

---
+++ database/migrations/1647577415756_roles_permissions.ts
@@ -0,0 +1,37 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import Logger from '@ioc:Adonis/Core/Logger'
+
+export default class RolesPermissions extends BaseSchema {
+  protected tableName = 'roles_permissions'
+
+  public async up() {
+    if (!(await this.schema.hasTable(this.tableName)))
+      this.schema.createTable(this.tableName, (table) => {
+        table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+
+        table
+          .uuid('role_id')
+          .references('id')
+          .inTable('roles')
+          .notNullable()
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE')
+
+        table
+          .uuid('permission_id')
+          .references('id')
+          .inTable('permissions')
+          .notNullable()
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE')
+
+        table.timestamp('created_at', { useTz: true })
+        table.timestamp('updated_at', { useTz: true })
+      })
+    else Logger.info('RolesPermissions migration already running')
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
