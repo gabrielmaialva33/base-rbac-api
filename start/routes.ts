333349688b5b515e3f087474843f1e07f140a869
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-03-17 23:07:19 -0300

    feat: implement health check route

--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,10 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
+Route.get('/', async ({ response }: HttpContextContract) => {
+  const report = await HealthCheck.getReport()
+  return report.healthy ? response.ok(report) : response.badRequest(report)
 })
