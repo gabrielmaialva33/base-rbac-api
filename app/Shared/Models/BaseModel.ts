c7060bf2ec2984e6866212655307fdce90f170a1
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-04-28 21:15:14 -0300

    refactor: add validation is deleted colunm

--- app/Shared/Models/BaseModel.ts
+++ app/Shared/Models/BaseModel.ts
@@ -13,13 +13,14 @@ export default class BaseModel extends BaseAdonisModel {
   @beforeFind()
   @beforeFetch()
   public static async ignoreDeleted(query: ModelQueryBuilderContract<any>): Promise<void> {
-    query.whereNot('is_deleted', true)
+    if (query.model.$getColumn('is_deleted')) query.whereNot('is_deleted', true)
   }
 
   @beforePaginate()
   public static ignoreDeletedPaginate(
     queries: [countQuery: ModelQueryBuilderContract<any>, query: ModelQueryBuilderContract<any>]
   ) {
-    for (const query of queries) query.whereNot('is_deleted', true)
+    for (const query of queries)
+      if (query.model.$getColumn('is_deleted')) query.whereNot('is_deleted', true)
   }
 }
